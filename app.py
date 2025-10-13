fe1b2775e99c54332f2c783dccd7d049576bd8e8
humaral <hmaoficial@gmail.com>
2025-10-13 15:19:19 +0200

    Reestructuracion del formato y contenido del login, header y footer para aprovechar los layouts de jinja.  Páginas estáticas bastante completas del login, header y footer.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, jsonify
 import os
+from datetime import datetime
+
+APP_VERSION = "0.1"
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_globals():
+    return {
+        "current_date": datetime.now().strftime('%d/%m/%Y'),
+        "current_year": datetime.now().year,
+        "version": APP_VERSION
+    }
 
 @app.route('/')
-def init_app(name=None):
+def inicio(name=None):
     DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
     return render_template('index.html', person=name, database=DATABASE)
 
